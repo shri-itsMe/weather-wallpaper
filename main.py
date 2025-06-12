c4edc00745b9ec520c80856c43e25baf78e53606
shriya-nc <shri.nallanc@gmail.com>
2025-06-11 22:08:49 -0400

    Added Default Method
    Now can reset to the original user's desktop

--- main.py
+++ main.py
@@ -8,20 +8,25 @@ loc = Location()
 print(loc.get_city())
 weather = Weather(location=loc.get_city())
 wallpaper = Wallpaper(weather=weather.current_condition())
+wallpaper.capture_wallpaper()
 
 print(weather.current_condition())
 print(wallpaper.get_wallpaper())
 wallpaper.set_wallpapers()
 
+# print(wallpaper.user_default())
+
+if input("change back? T or F: ") == "T":
+    wallpaper.user_default()
 
 
 
 # print(data["current"]["temp_f"])
 # print(data["current"]["condition"]["text"])
 
-#TODO: Make it run every hour
-#TODO: Get a reset control
-#TODO: Use an API to fetch wallpapers
+#TODO: Make it run every hour DONE
+#TODO: Get a reset control DONE
+#TODO: Use an API to fetch wallpapers 
 #TODO: how should I get the absolute file path for other people
 #TODO: Bundle into Platypus
 
--- wallpaper.py
+++ wallpaper.py
@@ -1,17 +1,39 @@
 #Set up wallpaper changin with osscript or apple script
 import subprocess
+import os
+import json
 
 class Wallpaper:
     def __init__(self, weather):
         self.weather = weather
         print("Init, weather = " + str(self.weather))
 
+    def capture_wallpaper(self):
+        
+        #Get file of wallpaper
+        script = f'''
+            tell application "System Events"
+                get picture of current desktop
+            end tell
+        '''
+        default_wallpaper = subprocess.run(['osascript', '-e', script], capture_output=True, text=True)
+        #save the absolute path for the wallpaper
+        config_path = os.path.abspath("./assets/config.json")
+        #save the abd path in a dict
+        data = {"user_wallpaper": default_wallpaper.stdout.strip()}
+
+        #put all of that in a json
+        with open(config_path, "w") as f:
+            json.dump(data, f, indent=2)
+        
+        return True
+
     def get_wallpaper(self):
         print("Running find_wallpaper, weather = " + str(self.weather))
         if self.weather == "Sunny":
             wallpaper = "Sunny"
     
-        elif self.weather == "Partly Cloudy":
+        elif self.weather == "Partly cloudy":
             wallpaper = "Partly_Cloudy"
     
         elif self.weather == "Rainy":
@@ -36,7 +58,7 @@ class Wallpaper:
         #     end tell
         script = f'''
             tell application "System Events"
-                set picture of every desktop to "/Users/shriya/weather-wallpaper/assets/{asset_name}.png"
+                set picture of current desktop to "/Users/shriya/weather-wallpaper/assets/{asset_name}.png"
             end tell
         '''
 
@@ -44,4 +66,24 @@ class Wallpaper:
     
     def test_set_wallpapers(self):
         return str(self.get_wallpaper())
+    
+    def user_default(self):
 
+        assets_folder = os.path.join(os.path.dirname(__file__), "assets")
+        config_path = os.path.join(assets_folder, "config.json")
+
+        with open(config_path) as default_wp_file:
+            parsed_json = json.load(default_wp_file)
+        default_wp_path = parsed_json["user_wallpaper"]
+        print(default_wp_path)
+
+
+        script = f'''
+            tell application "System Events"
+                set picture of current desktop to "{default_wp_path}"
+            end tell
+        '''
+        subprocess.run(['osascript', '-e', script])
+
+
+        return default_wp_path
